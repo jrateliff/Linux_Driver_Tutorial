4584edb88360c3fb44ff42f1bde86f95942b6e45
JTRDEV <jtrdevgit@gmail.com>
2025-03-10 07:41:24 -0400

    First commit

--- 01_hello/hello.c
+++ 01_hello/hello.c
@@ -1,18 +1,23 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-int my_init(void)
+static int __init my_init(void);
+static void my_exit(void);
+
+static int my_init(void)
 {
 	printk("hello - Hello, Kernel!\n");
 	return 0;
 }
 
-void my_exit(void)
+static void __exit my_exit(void)
 {
-	printk("hello - Goodbye, Kernel!\n");
+	printk("hello man - Goodbye man, Kernel!\n");
 }
 
 module_init(my_init);
 module_exit(my_exit);
 
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("James Rateliff");
+MODULE_DESCRIPTION("A simple Hello WOrld Linux Kernel Module");
